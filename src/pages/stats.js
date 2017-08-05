00bf0dacbea8f1f80b0395c05d247b50bec08ada
Colin <colinwmackenzie@gmail.com>
2017-08-05 12:00:49 -0400

    added icon

--- src/pages/stats.js
+++ src/pages/stats.js
@@ -1,5 +1,14 @@
 import React from 'react'
+import styled from 'styled-components'
 import data from '../../data/data.js'
+
+const FlexDiv = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 75%;
+`
+
+const FlexSvg = styled.svg`flex-basis: calc(100% / 15);`
 
 class Icon extends React.Component {
   constructor() {
@@ -7,14 +16,16 @@ class Icon extends React.Component {
   }
   render() {
     return (
-      <div
-        style={{
-          height: '40px',
-          width: '35px',
-          margin: '2px',
-          backgroundColor: this.props.color,
-        }}
-      />
+      <FlexSvg viewBox="0 0 1100 1100" width="100%" height="100%">
+        <path
+          style={{ fill: this.props.color }}
+          d="M219.5 580c.3-.3.4-.7.6-1l237-237c23.2-23.2 61-23.2 84.2 0l237 237c1 1 1.3 2.4 2.3 3.4l38.1 38.1 179.9-179.9c-53.8-148-171.3-266.2-320-319.3-55.8-20.6-116.2-31.9-179.3-31.9-62.4 0-122.8 10.6-178.6 30.5C171.3 174.5 53.7 291.3 0 440.7l179.2 179.6 40.3-40.3z"
+        />
+        <path
+          style={{ fill: this.props.color }}
+          d="M743.1 693.7l-29.6-29.6c-.8-.9-1-2-1.9-2.8l-180-180.1c-17.7-17.7-46.4-17.7-64.1 0l-66.3 66.3-.3-.3-145.4 145.7 31.7 31.7c.2.2.2.4.4.6l180.1 180.1c17.6 17.6 46.4 17.6 64.1 0l180-180.1c.1-.1.1-.2.2-.3l31.1-31.2z"
+        />
+      </FlexSvg>
     )
   }
 }
@@ -49,27 +60,23 @@ class StatsPage extends React.Component {
         </h1>
 
         <h2>Ballparks Visited</h2>
-        <div
-          style={{
-            display: 'flex',
-          }}
-        >
-          {icons}
-        </div>
+        <FlexDiv>
+          {' '}{icons}{' '}
+        </FlexDiv>
         <br />
         <p>
           {stats.visited.join(', ')}
         </p>
-        <h4>
+        <h4 style={{ color: '#666666' }}>
           {stats.remaining.length} Remaining
         </h4>
-        <p>
+        <p style={{ color: '#666666' }}>
           {stats.remaining.join(', ')}
         </p>
-        <h4>
+        <h4 style={{ color: '#9c0000' }}>
           {stats.retired.length} Retired Parks Visited
         </h4>
-        <p>
+        <p style={{ color: '#9c0000' }}>
           {stats.retired.join(', ')}
         </p>
       </div>
